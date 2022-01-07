6d47fac986edfd1631c6e853bbbcefd0fa628d94
Macbook <macbook@MacBook-Air-de-Macbook.local>
2022-01-06 22:53:01 -0500

    Cuarta parte tutorial implementado correctamente

--- shopping-cart/src/components/Phone.js
+++ shopping-cart/src/components/Phone.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { DragSource } from 'react-dnd';
+import { connect } from 'react-redux';
 import { ItemTypes } from './Constants';
+import { moveIncart } from '../actions/phones';
 
 
 // phone DnD spec
 const phoneSpec = {
     beginDrag(props){
-        console.log("begin drag")
         return{
-            name: props.brand 
+            name: props.brand,
+            id: props.id
+            
         }
     },
     endDrag(props, monitor, component){
         if (monitor.didDrop()){
-            const dragItem = monitor.getItem(); // from beginDrag
-            const dropResult = monitor.getDropResult();
-            // Move action goes here
+            const dragItem = monitor.getItem();
+            const dropResult = monitor.getDropResult();           
             console.log("You dropped ", dragItem.name, ' into '+ dropResult.name)
+             // Move action goes here
+            props.dispatch(moveIncart(dragItem.id))
         }else{
             return;
         }
@@ -60,4 +64,4 @@ class Phone extends Component{
     }
 }
 
-export default DragSource(ItemTypes.PHONE, phoneSpec, collect)(Phone);
+export default connect()(DragSource(ItemTypes.PHONE, phoneSpec, collect)(Phone));
